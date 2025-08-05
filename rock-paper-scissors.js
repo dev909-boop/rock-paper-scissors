fcafb8295a923a5be8c0a9cfd3c6f309acf626c8
dev909-boop <215330302+dev909-boop@users.noreply.github.com>
2025-08-05 12:43:02 -0400

    Generate random number between 1 and 3
    Created computerChoice variable which generates a number between 1 and 3, tested by priting the random number to the console

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,1 +1,8 @@
-console.log("Hello World!");
+//Variable to hold the computerChoice
+//Generate the computerChoice randomly from rock = 1, paper = 2, scissors = 3
+    //Math.floor(Math.random() * max - min + 1)) + min
+    
+let computerChoice = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
+console.log(computerChoice);
+
+//Create a function that returns the string "rock", "paper", or "scissors"
