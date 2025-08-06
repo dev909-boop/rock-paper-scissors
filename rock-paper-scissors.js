2085afd9bf9021afc2c88763b4265be52164cdb1
dev909-boop <215330302+dev909-boop@users.noreply.github.com>
2025-08-06 00:20:16 -0400

    Create function getComputerChoice
    Created a function to randomly generate either rock, paper, or scissor from the computer. Tested the result using console.log

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -2,7 +2,31 @@
 //Generate the computerChoice randomly from rock = 1, paper = 2, scissors = 3
     //Math.floor(Math.random() * max - min + 1)) + min
     
-let computerChoice = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
-console.log(computerChoice);
+
 
 //Create a function that returns the string "rock", "paper", or "scissors"
+function getComputerChoice(){
+
+    let computerChoice = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
+    console.log(computerChoice);
+
+    const rock = "rock";
+    const paper = "paper";
+    const scissors = "scissors";
+
+    if (computerChoice === 1) {
+        computerChoice = rock;
+    } else if (computerChoice === 2) {
+        computerChoice = paper;
+    } else {
+        computerChoice = scissors;
+    }
+
+    console.log(computerChoice)
+    return computerChoice;
+}
+
+getComputerChoice();
+
+
+
