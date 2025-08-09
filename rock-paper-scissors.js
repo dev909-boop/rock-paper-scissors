2982e544174af9099f6f9eb2bc0f26ad6deacab9
dev909-boop <215330302+dev909-boop@users.noreply.github.com>
2025-08-08 21:57:53 -0400

    Finalize game by correcting loop
    Fixed the bug in the loop so user is prompted each round for a selection, also added a scoreboard to keep track of the score through each round and then display the final score

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,29 +1,27 @@
 let humanScore = 0;
 let computerScore = 0;
+let draw = 0;
 
 const rock = "Rock";
 const paper = "Paper";
 const scissors = "Scissors";
-
-const computerSelection = getComputerChoice();  
-const humanSelection = getHumanChoice();
 
 //Create a function that returns the string "rock", "paper", or "scissors"
 function getComputerChoice(){
     //Variable to hold the computerChoice
     //Generate the computerChoice randomly from 3 options (rock, paper, scissors)
     let computerChoice = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
-    console.log(computerChoice);
+    //console.log(computerChoice);
 
     if (computerChoice === 1) {
         computerChoice = rock;
-        console.log(computerChoice);
+        console.log(`The computer chose: ${computerChoice}`);
     } else if (computerChoice === 2) {
         computerChoice = paper;
-        console.log(computerChoice);
+        console.log(`The computer chose: ${computerChoice}`);
     } else {
         computerChoice = scissors;
-        console.log(computerChoice);
+        console.log(`The computer chose: ${computerChoice}`);
     }
     return computerChoice
 }
@@ -37,29 +35,33 @@ function getHumanChoice(){
     //Assign humanChoice to variable
     if (humanChoice === "rock") {
         humanChoice = rock;
-        console.log(humanChoice);
+        console.log(`You chose: ${humanChoice}`);
     }else if (humanChoice === "paper") {
         humanChoice = paper;
-        console.log(humanChoice);
+        console.log(`You chose: ${humanChoice}`);
         
  //catches anything that is not rock or paper and assigns scissors, also handles null
     } else if (humanChoice === null) {
         humanChoice = scissors;
-        console.log(humanChoice);
+        console.log(`${humanChoice} was selected.`);
 
     }else {
-
         humanChoice = scissors;
-        console.log(humanChoice);
+        console.log(`You chose: ${humanChoice}`);
     }
     return humanChoice;
 }
 
 function playGame() {
-    for (let round = 0; round <5; round++){
+    for (let round = 1; round < 6; round++){
         function playRound() {
+            
+            const computerSelection = getComputerChoice();  
+            const humanSelection = getHumanChoice();
+            
             if (computerSelection === humanSelection) {
                 console.log("It's a draw!");
+                ++draw;
             } else if (
                 computerSelection === rock && humanSelection === scissors || 
                 computerSelection === paper && humanSelection === rock ||
@@ -72,11 +74,23 @@ function playGame() {
                 console.log(`You Win! ${humanSelection} beats ${computerSelection}.`)
                 ++humanScore;
             }
-
+            console.log(`~Scoreboard Round ${round}~ \nComputer: ${computerScore}, You: ${humanScore} \nDraw: ${draw}`);
         }
         playRound();
-        console.log(`~Scoreboard~ \nComputer: ${computerScore}, You: ${humanScore}`);
-    } 
+        
+    }
+
+    console.log(`FINAL SCORE: \n~Scoreboard~ \nComputer: ${computerScore}, You: ${humanScore} \nDraw: ${draw}`);
+    
+    if (computerScore > humanScore) {
+        console.log("Computer wins. Better luck next time!");
+    } else if (humanScore > computerScore) {
+        console.log("You won!");
+    } else {
+        console.log("It's a draw.");
+    }
+    
+    
     
 }
 
